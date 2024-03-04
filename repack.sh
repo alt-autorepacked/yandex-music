651fafbc8507cc31820dcc3c52bfcb93b4a88279
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-03-04 19:04:26 +0300

    refactor

--- repack.sh
+++ repack.sh
@@ -5,19 +5,17 @@ epm tool eget https://raw.githubusercontent.com/alt-autorepacked/common/v0.3.0/c
 # . ../common/common.sh
 
 _package="yandex-music"
-
+_repo="cucumber-sp/yandex-music-linux"
 arch="$(epm print info --debian-arch)"
-
 _suffix="*_$arch.deb"
 
 _download() {
-    _repo="cucumber-sp/yandex-music-linux"
     url=$(epm tool eget --list --latest https://github.com/$_repo/releases "$_suffix")
     real_download_url=$(epm tool eget --get-real-url $url)
     epm -y repack $real_download_url
 }
 
-download_version=$(_check_version_from_github "cucumber-sp/yandex-music-linux" "$_suffix")
+download_version=$(_check_version_from_github $_repo "$_suffix")
 remote_version=$(_check_version_from_remote)
 
 if [ "$remote_version" != "$download_version" ]; then
